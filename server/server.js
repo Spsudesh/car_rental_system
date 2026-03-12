78cdc6e8b736afe5534765fad6828a8edf6454a1
Sudesh Pol <sudeshpol18@gmail.com>
2026-03-13 00:28:08 +0530

    updated CORS code

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from"./configs/db.js";
 import userRouter from"./routes/userRoutes.js";
 import ownerRouter from"./routes/ownerRoutes.js";
 import carRouter from"./routes/carRoutes.js";
+import cors from"cors";
 
 // Initialize express app
 const app=express();
@@ -12,14 +13,22 @@ const app=express();
 // connect db
 await connectDB();
 
-//middleware
-app.use(cors());
+
+// middleware
+app.use(cors({
+  origin: 'https://car-rental-system-4xa4.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json({ limit: '10mb' }));
 
 app.get("/", (req, res) => res.send("server is running"));
 app.use('/api/user', userRouter)
 app.use('/api/owner',ownerRouter)
 app.use('/api/car', carRouter)
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
